75fec564d6fa52ddf77df8bc889c67ba3c229cb4
unknown <har33dik@yahoo.com>
2020-05-03 21:00:30 +0530

    fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,15 @@
+memory = {}
+
+function getNthFib(n) {
+	if(memory[n]!==undefined){
+		return memory[n];
+	}
+	if(n<=1){
+		return n
+	}
+	let val = getNthFib(n-1) + getNthFib(n-2)
+	memory[n]=val;
+	return val;
+}
+
+console.log(getNthFib(8))
